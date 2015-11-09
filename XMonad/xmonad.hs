0c85d98bf4cb6fa2d3cd2fae483025d243a69fe0
Reyu Zenfold <reyu@reyuzenfold.com>
2015-11-09 18:16:15 +0000

    XMonad: Rewrite and upgrade of main configuration

--- XMonad/xmonad.hs
+++ XMonad/xmonad.hs
@@ -8,6 +8,7 @@ import Control.Applicative ((<$>))
 import XMonad.Actions.Submap
 import XMonad.Actions.SpawnOn
 import XMonad.Actions.PerWorkspaceKeys
+import XMonad.Actions.PhysicalScreens
 import XMonad.Util.Run
 import XMonad.Util.NamedWindows (getName)
 import qualified XMonad.Prompt    as P
@@ -30,33 +31,61 @@ import XMonad.Layout.Tabbed
 import XMonad.Layout.PerWorkspace(onWorkspace)
 import XMonad.Layout.Grid
 import Data.Ratio ((%))
+import System.Posix.Unistd
+import XMonad.Util.EZConfig
 
+data Host = Desktop | Laptop Bool -- ^ Does the laptop have a Windows key?
+    deriving (Eq, Show, Read)
 
--- The preferred terminal program
+getHost :: IO Host
+getHost = do
+    hostName <- nodeName `fmap` getSystemID
+    return $ case hostName of
+        "renard" -> Desktop
+        "vulpie" -> Desktop
+        "crevan" -> Laptop True
+        _        -> Desktop
+
+main = do
+    host <- getHost
+    logPipe <- spawnPipe myBar
+    xmonad $ myConfig host logPipe
+
+myConfig host logPipe = defaultConfig
+    { terminal           = myTerminal
+    , focusFollowsMouse  = False
+    , borderWidth        = 2
+    , modMask            = if host == Laptop False
+                              then modMask defaultConfig
+                              else mod4Mask
+    , workspaces         = myWorkspaces
+    , normalBorderColor  = solarizedBackground
+    , focusedBorderColor = solarizedForeground
+    , layoutHook         = myLayoutHook
+    , manageHook         = myManageHook
+                           <+> manageSpawn
+                           <+> manageDocks
+                           <+> manageHook defaultConfig
+    , logHook            = myLoghook logPipe
+                           <+> logHook defaultConfig
+    , startupHook        = myStartupHook host logPipe
+                           <+> startupHook defaultConfig
+    } `additionalKeysP` myKeys host logPipe
+
+solarizedForeground = "#002b36"
+solarizedBackground = "#657b83"
 myTerminal = "st"
 
--- Whether focus follows the mouse pointer.
-myFocusFollowsMouse = False
-
--- Whether clicking on a window to focus also passes the click to the window
-myClickJustFocuses = False
-
--- Width of the window border in pixels.
-myBorderWidth = 2
-
--- Set modMask to left-alt
-myModMask = mod4Mask
-
 -- Key binding to toggle the gap for the bar.
-toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+-- toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 ------------------------------------------------------------
 -- The default number of workspaces (virtual screens) and their names.
 myWorkspaces = show <$> [1..10]
 
 -- Border colors for unfocused and focused windows, respectively.
-myNormalBorderColor  = "#002b36"
-myFocusedBorderColor = "#657b83"
+myNormalBorderColor  = solarizedBackground
+myFocusedBorderColor = solarizedForeground
 
 speakersAlsaName = "pci-0000_00_1b.0.analog-surround-51"
 headphonesAlsaName = "usb-Logitech_Logitech_G930_Headset-00.iec958-stereo"
@@ -66,79 +95,50 @@ adjustMute device value = spawn $ "pactl -- set-sink-mute alsa_output." ++ devic
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
-myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-    [ ((0                 ,  0x1008FF10  ), spawn "sudo systemctl suspend")
-    -- Headphones
-    , ((0                 ,  0x1008FF11  ), adjustVolume headphonesAlsaName "-5%")
-    , ((0                 ,  0x1008FF12  ), adjustMute headphonesAlsaName "toggle")
-    , ((0                 ,  0x1008FF13  ), adjustVolume headphonesAlsaName "+5%")
-    -- Speakers
-    , ((shiftMask         ,  0x1008FF11  ), adjustVolume speakersAlsaName "-5%")
-    , ((shiftMask         ,  0x1008FF12  ), adjustMute speakersAlsaName "toggle")
-    , ((shiftMask         ,  0x1008FF13  ), adjustVolume speakersAlsaName "+5%")
-    -- , ((0                 ,  0x1008FF14  ), spawn "/usr/bin/mpc -q toggle")
-    -- , ((0                 ,  0x1008FF16  ), spawn "/usr/bin/mpc -q prev")
-    -- , ((0                 ,  0x1008FF17  ), spawn "/usr/bin/mpc -q next")
-    , ((modm              ,  xK_BackSpace), focusUrgent)
-    , ((modm .|. shiftMask,  xK_BackSpace), clearUrgents)
-    , ((modm              ,  xK_Return   ), windows W.swapMaster)
-    , ((modm .|. shiftMask,  xK_Return   ), spawnHere (myTerminal ++ " -e tmux"))
-    , ((modm              ,  xK_Tab      ), windows W.focusDown)
-    , ((modm              ,  xK_space    ), sendMessage NextLayout)
-    , ((modm .|. shiftMask,  xK_space    ), setLayout $ XMonad.layoutHook conf)
-    , ((modm .|. shiftMask,  xK_c        ), kill)
-    , ((modm              ,  xK_h        ), sendMessage Shrink)
-    , ((modm              ,  xK_j        ), windows W.focusDown)
-    , ((modm .|. shiftMask,  xK_j        ), windows W.swapDown  )
-    , ((modm              ,  xK_k        ), windows W.focusUp  )
-    , ((modm .|. shiftMask,  xK_k        ), windows W.swapUp    )
-    , ((modm              ,  xK_l        ), sendMessage Expand)
-    , ((modm .|. mod1Mask ,  xK_l        ), spawn "/usr/bin/xscreensaver-command -activate")
-    , ((modm              ,  xK_m        ), windows W.focusMaster)
-    , ((modm .|. mod1Mask ,  xK_m        ), unsafePrompt "/usr/bin/mpc" mpcXPConfig)
-    , ((modm              ,  xK_n        ), unsafePrompt (myTerminal ++ " -t") myXPConfig )
-    , ((modm              ,  xK_p        ), spawn "~/bin/passmenu" )
-    , ((modm              ,  xK_q        ), spawn "xmonad -- recompile; xmonad -- restart")
-    , ((modm .|. shiftMask,  xK_q        ), io exitSuccess)
-    , ((modm              ,  xK_r        ), runOrRaisePrompt myXPConfig )
-    , ((modm              ,  xK_s        ), unsafePrompt myTerminal myXPConfig )
-    , ((modm .|. shiftMask,  xK_s        ), sshPrompt myXPConfig )
-    , ((modm              ,  xK_t        ), withFocused $ windows . W.sink)
-    , ((modm              ,  xK_u        ), bindOn
-        [ ("5", spawnHere   "/usr/bin/firefox -P alternate -new-window")
-        , ("" , spawnHere "/usr/bin/firefox -P default -new-window")
-        ])
-    , ((modm .|. shiftMask,  xK_v        ), sendMessage (IncMasterN (-1)))
-    , ((modm              ,  xK_w        ), windowPromptGoto myXPConfig )
-    , ((modm .|. shiftMask,  xK_w        ), sendMessage (IncMasterN 1))
+myKeys host logPipe = myKeymap host (myConfig host logPipe)
+myKeymap host conf =
+    [
+    -- Volume: Headphones
+      ("<XF86AudioLowerVolume>", adjustVolume headphonesAlsaName "5%-")
+    , ("<XF86AudioMute>", adjustMute headphonesAlsaName "toggle")
+    , ("<XF86AudioRaiseVolume>", adjustVolume headphonesAlsaName "5%+")
+    -- Volume: Speakers
+    , ("S-<XF86AudioLowerVolume>", adjustVolume speakersAlsaName "5%-")
+    , ("S-<XF86AudioMute>", adjustMute speakersAlsaName "toggle")
+    , ("S-<XF86AudioRaiseVolume>", adjustVolume speakersAlsaName "5%+")
+    -- General
+    , ("M-<Backspace>", focusUrgent)
+    , ("M-S-<Backspace>", clearUrgents)
+    , ("M-<Return>", windows W.swapMaster)
+    , ("M-S-<Return>", spawnHere (myTerminal ++ " tmux new-session"))
+    , ("M-<Space>", sendMessage NextLayout)
+    , ("M-h", sendMessage Shrink)
+    , ("M-l", sendMessage Expand)
+    , ("M-M1-l", spawn "/usr/bin/xscreensaver-command -activate")
+    , ("M-m", windows W.focusMaster)
+    , ("M-n", unsafePrompt (myTerminal ++ " -t") myXPConfig )
+    , ("M-p", spawn "~/bin/passmenu" )
+    , ("M-q", spawn "xmonad -- recompile; xmonad -- restart")
+    , ("M-S-q", io exitSuccess)
+    , ("M-r", runOrRaisePrompt myXPConfig )
+    , ("M-s", unsafePrompt myTerminal myXPConfig )
+    , ("M-S-s", sshPrompt myXPConfig )
+    , ("M-t", withFocused $ windows . W.sink)
+    , ("M-u", spawnHere "/usr/bin/firefox -P default -new-window")
+    , ("M-S-v", sendMessage (IncMasterN (-1)))
+    , ("M-w", windowPromptGoto myXPConfig )
+    , ("M-S-w", sendMessage (IncMasterN 1))
     ]
     ++
-
-    --
-    -- mod-[F1..F10], Switch to workspace N
-    -- mod-shift-[F1..F10], Move client to workspace N
-    --
-    [((m .|. modm, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_F1..xK_F10]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+    [(m ++ "M-" ++ k, windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) ["<F" ++ show i ++ ">" | i <- [1..]]
+        , (f, m) <- [(W.greedyView, ""), (W.shift, "S-")]
     ]
     ++
-
-    --
-    -- mod-{',','.'}, Switch to physical/Xinerama screens 1 or 2
-    -- mod-shift-{',','.'}, Move client to screen 1 or 2
-    --
-    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_comma, xK_period] [1,0]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+    [(m ++ "M-" ++ k, f s)
+        | (k, s) <- zip [",","."] [0..]
+        , (f, m) <- [(viewScreen, ""), (sendToScreen, "S-")]
     ]
-
--- Non-numeric num pad keys, sorted by number
-numPadKeys = [ xK_KP_Home, xK_KP_Up,    xK_KP_Page_Up   -- 1, 2, 3
-             , xK_KP_Left, xK_KP_Begin, xK_KP_Right     -- 4, 5, 6
-             , xK_KP_End,  xK_KP_Down,  xK_KP_Page_Down -- 7, 8, 9
-             , xK_KP_Insert]                            -- 0
-
 
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
@@ -155,19 +155,18 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList
 ------------------------------------------------------------------------
 -- Layouts:
 --
-myLayoutHook  = avoidStruts $
-                smartBorders $
-                onWorkspace "10" imLayout
-                standardLayouts
+myLayoutHook = avoidStrutsOn [U] $
+               smartBorders $
+               onWorkspace "10" imLayout
+               standardLayouts
     where
         standardLayouts = noBorders Full ||| tiled |||  reflectTiled ||| Mirror tiled ||| Grid
         tiled           = ResizableTall 1 (2/100) (1/2) []
         reflectTiled    = reflectHoriz tiled
-        imLayout        = withIM (1%8) psiRoster $ reflectHoriz $
-                          withIM (1%9) skypeRoster chatLayouts
+        imLayout        = withIM (1%9) skypeRoster chatLayouts
         chatLayouts     = tabbed shrinkText tabConfig ||| Grid ||| tiled
-        psiRoster       = ClassName "psi"    `And` Resource "main"
         skypeRoster     = ClassName "Skype"  `And` Not (Role  "ConversationsWindow")
+
 
 ------------------------------------------------------------------------
 -- Window rules:
@@ -194,7 +193,8 @@ myManageHook = composeAll . concat $
 ------------------------------------------------------------------------
 -- Startup hook
 --
-myStartupHook =
+myStartupHook host logPipe = do
+    checkKeymap (myConfig host logPipe) (myKeys host logPipe)
     spawn "tmux new-session -d -s Global weechat \\; set-option -t Global destroy-unattached off"
 
 ------------------------------------------------------------------------
@@ -210,9 +210,10 @@ myBar = "dzen2" ++ concatMap (" " ++)
     , "-bg '#002b36'"
     , "-fg '#657b83'"
     , "-ta 'left'"
+    , "-e 'onstart=lower'"
     ]
 
-myPP = dzenPP
+myLoghook h = dynamicLogWithPP $ defaultPP 
     { ppCurrent         = dzenColor "#859900" "" . wrap "<" "> "
     , ppVisible         = dzenColor "#2AA198" "" . wrap "[" "] "
     , ppHidden          = dzenColor "#93A1A1" "" . wrap "" " "
@@ -253,24 +254,3 @@ instance UrgencyHook LibNotifyUrgencyHook where
         name     <- getName w
         Just idx <- W.findTag w <$> gets windowset
         safeSpawn "notify-send" [show name, "workspace " ++ idx]
-
-
-------------------------------------------------------------------------
--- Now run xmonad with all the defaults we set up.
-main = xmonad =<< statusBar myBar myPP toggleStrutsKey (withUrgencyHook LibNotifyUrgencyHook defaults)
-defaults = defaultConfig
-    { terminal           = myTerminal
-    , focusFollowsMouse  = myFocusFollowsMouse
-    , clickJustFocuses   = myClickJustFocuses
-    , borderWidth        = myBorderWidth
-    , modMask            = myModMask
-    , workspaces         = myWorkspaces
-    , normalBorderColor  = myNormalBorderColor
-    , focusedBorderColor = myFocusedBorderColor
-    , keys               = myKeys
-    , mouseBindings      = myMouseBindings
-    , layoutHook         = myLayoutHook
-    , manageHook         = manageSpawn <+> myManageHook
-    , startupHook        = myStartupHook
-    }
--- vim: set makeprg=xmonad\ --recompile
