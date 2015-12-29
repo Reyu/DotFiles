3abf531aa6b1f3cbdc0839d915616e6efc15c96c
Tim Millican <tim.millican@linux.com>
2015-12-29 21:24:25 +0000

    XMonad: Add key to toggle struts

--- XMonad/xmonad.hs
+++ XMonad/xmonad.hs
@@ -211,6 +211,7 @@ myKeymap host conf =
     , ("M-u", spawnHere "/usr/bin/firefox -P default -new-window")
     , ("<Print>", spawn "scrot")
     , ("C-<Print>", spawn "sleep 0.2; scrot -s")
+    , ("M-b", sendMessage $ ToggleStruts)
     -- , ("M-S-t", spawn "stoken-type")
     -- Various Prompts
     , ("M-p p", spawn "~/bin/passmenu" )
