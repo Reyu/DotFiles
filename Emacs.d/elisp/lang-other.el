f2ec0eccc05723ae8edd10baf9f05ab0c5f1fb3a
Reyu Zenfold <reyu@reyuzenfold.com>
2017-03-29 18:42:12 +0000

    Emacs: Add salt-mode
    Also adds new `lang-other` section, for misc. language packages.

---
+++ Emacs.d/elisp/lang-other.el
@@ -0,0 +1,6 @@
+;; salt-mode configuration
+;; https://github.com/glynnforrest/salt-mode
+(use-package salt-mode
+  :ensure t)
+
+(provide 'lang-other)
--- Emacs.d/init.el
+++ Emacs.d/init.el
@@ -22,3 +22,4 @@
 (require 'lang-python)
 (require 'lang-web)
 (require 'lang-haskell)
+(require 'lang-other)
