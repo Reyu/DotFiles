90abee6f76f861b61bfdd19b2f333a4bc17e0262
Reyu Zenfold <reyu@reyuzenfold.com>
2017-09-07 14:13:54 +0000

    Emacs: Fix org-mode not using fixed width font
    Was being overided by theme

--- Emacs.d/elisp/base-theme.el
+++ Emacs.d/elisp/base-theme.el
@@ -2,6 +2,9 @@
   :defer t
   :if window-system
   :init
-  (load-theme 'solarized-dark t))
+  (load-theme 'solarized-dark t)
+  :config
+  (setq solarized-use-variable-pitch nil
+	solarized-scale-org-headlines nil))
 
 (provide 'base-theme)
